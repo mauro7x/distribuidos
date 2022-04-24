e150cba55a3f76d8d5fbafb3b5cb9a95c521f577
Mauro Parafati <mparafati@fi.uba.ar>
2022-04-24 01:27:53 -0300

    Add basic event_receiver

---
+++ TP1/services/event_receiver/src/event_dispatcher/constants.rs
@@ -0,0 +1,10 @@
+// Paths
+pub const CONFIG_FILE_PATH: &str = "config/dispatcher.json";
+
+// Env vars
+pub const THREAD_POOL_SIZE_ENV: &str = "DISPATCHER_THREAD_POOL_SIZE";
+pub const QUEUE_SIZE_ENV: &str = "DISPATCHER_QUEUE_SIZE";
+
+// Defaults
+pub const DEFAULT_THREAD_POOL_SIZE: usize = 3;
+pub const DEFAULT_QUEUE_SIZE: usize = 10;
