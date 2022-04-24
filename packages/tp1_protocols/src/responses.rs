a3dd973e23d81d6a06d4d94868ddd632dc08a09e
Mauro Parafati <mparafati@fi.uba.ar>
2022-04-24 20:53:02 -0300

    Rename methoods

--- packages/tp1_protocols/src/responses.rs
+++ packages/tp1_protocols/src/responses.rs
@@ -6,25 +6,25 @@ use std::{
 
 // Send
 
-pub fn send_event_received_res(stream: &mut TcpStream) -> Result<(), Error> {
+pub fn send_event_received(stream: &mut TcpStream) -> Result<(), Error> {
     stream.write_all(&[OP_EVENT_RECEIVED])?;
 
     Ok(())
 }
 
-pub fn send_invalid_format_res(stream: &mut TcpStream) -> Result<(), Error> {
+pub fn send_invalid_format(stream: &mut TcpStream) -> Result<(), Error> {
     stream.write_all(&[OP_INVALID_FORMAT])?;
 
     Ok(())
 }
 
-pub fn send_server_at_capacity_res(stream: &mut TcpStream) -> Result<(), Error> {
+pub fn send_server_at_capacity(stream: &mut TcpStream) -> Result<(), Error> {
     stream.write_all(&[OP_SERVER_AT_CAPACITY])?;
 
     Ok(())
 }
 
-pub fn send_internal_server_error_res(stream: &mut TcpStream) -> Result<(), Error> {
+pub fn send_internal_server_error(stream: &mut TcpStream) -> Result<(), Error> {
     stream.write_all(&[OP_INTERNAL_SERVER_ERROR])?;
 
     Ok(())
