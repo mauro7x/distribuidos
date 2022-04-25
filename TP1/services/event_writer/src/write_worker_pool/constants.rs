2669ab703bd3e841e4253a575e794b51efeb7ac4
Mauro Parafati <mparafati@fi.uba.ar>
2022-04-25 04:07:29 -0300

    Add db writer logic in Event Writer (#88)
    * Bump protocols
    
    * Implement logic for Event Writer

--- TP1/services/event_writer/src/write_worker_pool/constants.rs
+++ TP1/services/event_writer/src/write_worker_pool/constants.rs
@@ -5,8 +5,12 @@ pub const CONFIG_FILE_PATH: &str = "config/workers.json";
 pub const FLUSH_TIMEOUT_MS_ENV: &str = "FLUSH_TIMEOUT_MS";
 pub const WORKER_POOL_SIZE_ENV: &str = "WORKER_POOL_SIZE";
 pub const WORKER_QUEUE_SIZE_ENV: &str = "WORKER_QUEUE_SIZE";
+pub const PARTITION_SECS_ENV: &str = "PARTITION_SECS";
+pub const DATABASE_PATH_ENV: &str = "DATABASE_PATH";
 
 // Defaults
 pub const DEFAULT_FLUSH_TIMEOUT_MS: u64 = 100;
 pub const DEFAULT_WORKER_POOL_SIZE: usize = 3;
 pub const DEFAULT_WORKER_QUEUE_SIZE: usize = 10;
+pub const DEFAULT_PARTITION_SECS: u32 = 600; // 10 min
+pub const DEFAULT_DATABASE_PATH: &str = "/events";
