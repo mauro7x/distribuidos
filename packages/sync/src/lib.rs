a4a41fd78bf4340cb1312aacd311e939038ad385
Mauro Parafati <mparafati@fi.uba.ar>
2022-04-24 21:16:22 -0300

    Add blocking SingleWorker

--- packages/sync/src/lib.rs
+++ packages/sync/src/lib.rs
@@ -4,6 +4,7 @@ mod thread_pool;
 mod types;
 mod worker_pool;
 
-pub use self::single_worker::SingleWorker;
+pub use self::single_worker::blocking::SingleWorker;
+pub use self::single_worker::timeout::SingleWorker as SingleWorkerTimeout;
 pub use self::thread_pool::{ExecuteError, ThreadPool};
 pub use self::worker_pool::{MessageSender, QueueError, WorkerPool};
