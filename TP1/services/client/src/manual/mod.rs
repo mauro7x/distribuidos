4acfd1c899ab50ab0d588496711d21afba4411d2
Mauro Parafati <mparafati@fi.uba.ar>
2022-04-27 03:44:31 -0300

    Fix query errors

--- TP1/services/client/src/manual/mod.rs
+++ TP1/services/client/src/manual/mod.rs
@@ -5,27 +5,27 @@ use crate::{
     parse::parse_row,
     types::{InputError, Row},
 };
-use distribuidos_tp1_gateway::{Gateway, SendQueryError};
-use distribuidos_tp1_protocols::types::Query;
+use distribuidos_tp1_gateway::Gateway;
+use distribuidos_tp1_protocols::types::{errors::QueryError, Query};
 use distribuidos_types::BoxResult;
 use std::io;
 
 use log::*;
 
-fn handle_errors(error: SendQueryError) -> BoxResult<()> {
+fn handle_errors(error: QueryError) -> BoxResult<()> {
     match error {
         // 4XX
-        SendQueryError::Invalid => warn!("Response: Invalid format (400)"),
-        SendQueryError::InvalidRange => warn!("Response: Invalid range (400)"),
-        SendQueryError::InvalidAggrWindow => warn!("Response: Invalid aggregation window (400)"),
-        SendQueryError::MetricNotFound => warn!("Response: Metric not found (404)"),
+        QueryError::Invalid => warn!("Response: Invalid format (400)"),
+        QueryError::InvalidRange => warn!("Response: Invalid range (400)"),
+        QueryError::InvalidAggrWindow => warn!("Response: Invalid aggregation window (400)"),
+        QueryError::MetricNotFound => warn!("Response: Metric not found (404)"),
         // 5XX
-        SendQueryError::ServerAtCapacity => error!("Response: Server at capacity (503)"),
-        SendQueryError::InternalServerError => {
+        QueryError::ServerAtCapacity => error!("Response: Server at capacity (503)"),
+        QueryError::InternalServerError => {
             error!("Response: Internal server error (500)")
         }
         // Self errors
-        SendQueryError::IOError(err) => return Err(err.into()),
+        QueryError::IOError(err) => return Err(err.into()),
     };
 
     Ok(())
