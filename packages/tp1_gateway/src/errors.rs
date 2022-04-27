b3c687599f99f6b5538b7de33a4a95c323477567
Mauro Parafati <mparafati@fi.uba.ar>
2022-04-27 00:57:25 -0300

    Fix gateway

--- packages/tp1_gateway/src/errors.rs
+++ packages/tp1_gateway/src/errors.rs
@@ -4,25 +4,25 @@ use distribuidos_tp1_protocols::types::errors::{QueryError, SendError};
 
 #[derive(Debug)]
 pub enum SendQueryError {
-    SendError(SendError),
-    QueryError(QueryError),
-    IOError(Error),
+    Send(SendError),
+    Query(QueryError),
+    IO(Error),
 }
 
 impl From<SendError> for SendQueryError {
     fn from(error: SendError) -> SendQueryError {
-        SendQueryError::SendError(error)
+        SendQueryError::Send(error)
     }
 }
 
 impl From<QueryError> for SendQueryError {
     fn from(error: QueryError) -> SendQueryError {
-        SendQueryError::QueryError(error)
+        SendQueryError::Query(error)
     }
 }
 
 impl From<Error> for SendQueryError {
     fn from(error: Error) -> SendQueryError {
-        SendQueryError::IOError(error)
+        SendQueryError::IO(error)
     }
 }
