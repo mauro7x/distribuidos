548cfbee67f8f0a1ca6c7537336c35045fddcb59
Mauro7x <mparafati@fi.uba.ar>
2022-04-28 12:22:47 -0300

    Remove old files

--- TP1/.old/aggregator/src/lib.rs
+++
@@ -1,4 +0,0 @@
-mod types;
-
-pub mod forker;
-pub mod server;
--- TP1/.old/aggregator/src/types.rs
+++
@@ -1,9 +0,0 @@
-use std::net::TcpStream;
-
-use distribuidos_tp1_protocols::types::Query;
-
-#[derive(Debug)]
-pub struct QueryRequest {
-    pub from: TcpStream,
-    pub query: Query,
-}
