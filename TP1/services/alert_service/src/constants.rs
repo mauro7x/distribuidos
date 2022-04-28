07da59bfbc71ae26acd967eee7163e34abc39fec
Mauro Parafati <mparafati@fi.uba.ar>
2022-04-28 06:05:38 -0300

    Add alert service (#98)
    * Dispatcher with CtrlC handler
    
    * Alert service

---
+++ TP1/services/alert_service/src/constants.rs
@@ -0,0 +1,9 @@
+// Paths
+pub const CONFIG_FILE_PATH: &str = "./config/dispatcher.json";
+pub const CHECKS_FILE_PATH: &str = "./config/checks.json";
+
+// Env vars
+pub const FREQUENCY_SECS_ENV: &str = "FREQUENCY_SECS";
+
+// Defaults
+pub const DEFAULT_FREQUENCY_SECS: u32 = 60;
---
+++ TP1/services/alert_service/src/lib.rs
@@ -0,0 +1,7 @@
+mod checks;
+mod config;
+mod constants;
+mod types;
+
+pub mod check_executor;
+pub mod dispatcher;
