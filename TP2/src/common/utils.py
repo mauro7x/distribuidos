057c1731816fd0149e66452d1f0db4aecb4fe693
Mauro Parafati <mparafati@fi.uba.ar>
2022-05-22 18:31:53 -0300

    Add env var for logging levels (#121)

--- TP2/src/common/utils.py
+++ TP2/src/common/utils.py
@@ -1,5 +1,9 @@
 import json
 import logging
+import os
+
+DEFAULT_LOGGING_LEVEL = logging.WARN
+LOGGING_LEVEL_ENV_KEY = 'LOG_LEVEL'
 
 
 def read_json(filepath):
@@ -8,8 +12,9 @@ def read_json(filepath):
     return data
 
 
-def init_log(logging_level: str = 'INFO'):
-    level = logging._nameToLevel.get(logging_level.upper(), logging.WARN)
+def init_log():
+    level_name = os.getenv(LOGGING_LEVEL_ENV_KEY, '')
+    level = logging._nameToLevel.get(level_name.upper(), logging.WARN)
     logging.basicConfig(
         format='%(asctime)s %(levelname)-8s %(message)s',
         level=level,
