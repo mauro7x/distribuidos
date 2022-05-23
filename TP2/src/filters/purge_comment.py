c9e1ff0a08ec91adfbeac13bd1ac291b9387cc42
Mauro7x <mparafati@fi.uba.ar>
2022-05-22 22:51:01 -0300

    Add hardcoded purgers (temp)

--- TP2/src/filters/purge_comment.py
+++ TP2/src/filters/purge_comment.py
@@ -3,15 +3,32 @@ from common.filters.custom import Filter
 from common.utils import init_log
 
 
-def comment_handler(context, send_fn, data):
+def comment_handler(_, send_fn, data):
     logging.debug(f'Handler called with: {data}')
+
+    if not data.permalink:
+        logging.debug('Ignoring comment with null URL')
+        return
+
+    if not data.sentiment:
+        logging.debug('Ignoring comment with null sentiment')
+        return
+
+    if not data.body:
+        logging.debug('Ignoring comment with null body')
+        return
+
+    send_fn({
+        "url": data.permalink,
+        "sentiment": data.sentiment,
+        "body": data.body
+    })
 
 
 def main():
     init_log()
     handlers = {"comment": comment_handler}
-    context = {}
-    filter = Filter(handlers, context)
+    filter = Filter(handlers)
     filter.run()
 
 
