bf933e0bdec7002f6ed2a104b51d07ad46b1a2db
Mauro7x <mparafati@fi.uba.ar>
2022-05-23 01:07:35 -0300

    Filter: extract_post_id

--- TP2/src/filters/extract_post_id.py
+++ TP2/src/filters/extract_post_id.py
@@ -2,16 +2,37 @@ import logging
 from common.filters.custom import Filter
 from common.utils import init_log
 
+# Regex method:
+# import re
+# REGEX = r'comments/([^/]*)'
+#
+# def extract_post_id(url):
+#     return re.findall(REGEX, url)[0]
 
-def comment_handler(context, send_fn, data):
-    logging.debug(f'Handler called with: {data}')
+
+# Split method:
+def extract_post_id(url):
+    # Format: protocol://host/r/meirl/comments/{POST_ID}/meirl/{comment_id}
+    # e.g.: https://old.reddit.com/r/meirl/comments/tswh3j/meirl/i2x2j0g/
+    # Splitting by '/', 7th group is our ID
+    return url.split('/')[6]
+
+
+def comment_handler(_, send_fn, data):
+    logging.info(f'Handler called with: {data}')
+    p_id = extract_post_id(data.url)
+
+    send_fn({
+        "p_id": p_id,
+        "sentiment": data.sentiment,
+        "body": data.body
+    })
 
 
 def main():
     init_log()
     handlers = {"comment": comment_handler}
-    context = {}
-    filter = Filter(handlers, context)
+    filter = Filter(handlers)
     filter.run()
 
 
